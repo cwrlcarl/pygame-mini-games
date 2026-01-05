346bf0012d8774e2be474f94e82e49a92b88bee7
cwrl <29carlganalon@gmail.com>
2026-01-05 21:25:14 +0800

    Display health text

--- falling-objects/main.py
+++ falling-objects/main.py
@@ -9,6 +9,7 @@ SCREEN_WIDTH, SCREEN_HEIGHT = 600, 400
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Catch the flying ahh object")
 screen_font = pygame.font.SysFont("Monocraft", 17)
+game_over_font = pygame.font.SysFont("Monocraft", 40)
 
 PLAYER_WIDTH, PLAYER_HEIGHT = 70, 15
 PLAYER_SPEED = 4
@@ -22,8 +23,10 @@ TEXT_COLOR = (255, 255, 255)
 PLAYER_COLOR = (224, 108, 152)
 OBJECT_COLOR = (255, 151, 33)
 
+game_over = False
 
-def draw_window(player_rect, object_rect, score):
+
+def draw_window(player_rect, object_rect, score, health):
     screen.fill(BG_COLOR)
 
     pygame.draw.rect(screen, PLAYER_COLOR, player_rect)
@@ -31,6 +34,14 @@ def draw_window(player_rect, object_rect, score):
 
     score_text = screen_font.render(f"Score: {score}", True, TEXT_COLOR)
     screen.blit(score_text, (15, 10))
+
+    health_text = screen_font.render(f"Health: {health}", True, TEXT_COLOR)
+    screen.blit(health_text, (15, 35))
+
+    if game_over:
+        game_over_text = game_over_font.render("Game Over!", True, TEXT_COLOR)
+        screen.blit(game_over_text,(SCREEN_WIDTH // 2 - game_over_text.get_width() // 2,
+                    SCREEN_HEIGHT // 2 - game_over_text.get_height() // 2))
 
     pygame.display.update()
 
@@ -44,17 +55,21 @@ def handle_movement(player_rect):
         player_rect.x += PLAYER_SPEED
 
 
-def handle_object(object_rect, player_rect, score):
+def handle_object(object_rect, player_rect, score, health):
     object_rect.y += FALL_SPEED
 
     if object_rect.y > SCREEN_HEIGHT:
+        health -= 1
         reset_object(object_rect)
+        
+        if health <= 0:
+            print("Game Over")
 
     if player_rect.colliderect(object_rect):
         score += 10
         reset_object(object_rect)
     
-    return score
+    return score, health
 
 
 def reset_object(object_rect):
@@ -64,10 +79,10 @@ def reset_object(object_rect):
 
 def main():
     score = 0
-    lives = 3
+    health = 3
     
     player_rect = pygame.Rect(((SCREEN_WIDTH // 2) - (PLAYER_WIDTH // 2),
-                                SCREEN_HEIGHT - PLAYER_HEIGHT,
+                                SCREEN_HEIGHT - PLAYER_HEIGHT - 20,
                                 PLAYER_WIDTH, PLAYER_HEIGHT))
     
     object_rect = pygame.Rect((X_POS, Y_POS,
@@ -81,9 +96,9 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
 
-        score = handle_object(object_rect, player_rect, score)
+        score, health = handle_object(object_rect, player_rect, score, health)
         handle_movement(player_rect)
-        draw_window(player_rect, object_rect, score)
+        draw_window(player_rect, object_rect, score, health)
         clock.tick(FPS)
 
     pygame.quit()
