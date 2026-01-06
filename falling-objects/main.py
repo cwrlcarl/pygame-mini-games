b86ab92e352b322e35a7d6d12de2f6a7d1d4c5b1
cwrl <29carlganalon@gmail.com>
2026-01-06 11:44:02 +0800

    Display game over text

--- falling-objects/main.py
+++ falling-objects/main.py
@@ -4,46 +4,54 @@ import random
 pygame.init()
 
 FPS = 60
-SCREEN_WIDTH, SCREEN_HEIGHT = 600, 400
+SCREEN_WIDTH, SCREEN_HEIGHT = 550, 400
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Catch the flying ahh object")
-screen_font = pygame.font.SysFont("Monocraft", 17)
-game_over_font = pygame.font.SysFont("Monocraft", 40)
+main_screen_font = pygame.font.SysFont("Monocraft", 17)
+game_over_font = pygame.font.SysFont("Monocraft", 80)
+subtext_font = pygame.font.SysFont("Monocraft", 13)
 
 PLAYER_WIDTH, PLAYER_HEIGHT = 70, 15
 PLAYER_SPEED = 4
 
 X_POS, Y_POS = random.randint(0, SCREEN_WIDTH - 50), -50
 RADIUS = 20
-FALL_SPEED = 3
+FALL_SPEED = 4
 
 BG_COLOR = (0, 0, 0)
-TEXT_COLOR = (255, 255, 255)
+MAIN_TEXT_COLOR = (255, 255, 255)
+SUBTEXT_COLOR = (171, 171, 171)
 PLAYER_COLOR = (224, 108, 152)
 OBJECT_COLOR = (255, 151, 33)
 
-game_over = False
 
-
-def draw_window(player_rect, object_rect, score, health):
+def draw_window(player_rect, object_rect, score, health, game_over):
     screen.fill(BG_COLOR)
 
     pygame.draw.rect(screen, PLAYER_COLOR, player_rect)
     pygame.draw.circle(screen, OBJECT_COLOR, object_rect.center, RADIUS)
 
-    score_text = screen_font.render(f"Score: {score}", True, TEXT_COLOR)
+    score_text = main_screen_font.render(f"Score: {score}", True, MAIN_TEXT_COLOR)
     screen.blit(score_text, (15, 10))
 
-    health_text = screen_font.render(f"Health: {health}", True, TEXT_COLOR)
+    health_text = main_screen_font.render(f"Health: {health}", True, MAIN_TEXT_COLOR)
     screen.blit(health_text, (15, 35))
 
     if game_over:
-        game_over_text = game_over_font.render("Game Over!", True, TEXT_COLOR)
-        screen.blit(game_over_text,(SCREEN_WIDTH // 2 - game_over_text.get_width() // 2,
-                    SCREEN_HEIGHT // 2 - game_over_text.get_height() // 2))
+        draw_game_over_screen()
 
     pygame.display.update()
+
+
+def draw_game_over_screen():
+    game_over_text = game_over_font.render(":(", True, MAIN_TEXT_COLOR)
+    screen.blit(game_over_text,((SCREEN_WIDTH // 2) - (game_over_text.get_width() // 2),
+                (SCREEN_HEIGHT // 2) - (game_over_text.get_height())))
+    
+    instructions_text = subtext_font.render("Press R to restart | ESC to quit", True, SUBTEXT_COLOR)
+    screen.blit(instructions_text, ((SCREEN_WIDTH // 2) - (instructions_text.get_width() // 2),
+                (SCREEN_HEIGHT // 2) + (instructions_text.get_height() + 10)))
 
 
 def handle_movement(player_rect):
@@ -55,21 +63,22 @@ def handle_movement(player_rect):
         player_rect.x += PLAYER_SPEED
 
 
-def handle_object(object_rect, player_rect, score, health):
+def handle_object(object_rect, player_rect, score, health, game_over):
     object_rect.y += FALL_SPEED
 
     if object_rect.y > SCREEN_HEIGHT:
         health -= 1
         reset_object(object_rect)
-        
+
         if health <= 0:
+            game_over = True
             print("Game Over")
 
     if player_rect.colliderect(object_rect):
         score += 10
         reset_object(object_rect)
     
-    return score, health
+    return score, health, game_over
 
 
 def reset_object(object_rect):
@@ -80,6 +89,7 @@ def reset_object(object_rect):
 def main():
     score = 0
     health = 3
+    game_over = False
     
     player_rect = pygame.Rect(((SCREEN_WIDTH // 2) - (PLAYER_WIDTH // 2),
                                 SCREEN_HEIGHT - PLAYER_HEIGHT - 20,
@@ -96,9 +106,12 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
 
-        score, health = handle_object(object_rect, player_rect, score, health)
+            if event.type == pygame.K_r:
+                running = False
+
+        score, health, game_over = handle_object(object_rect, player_rect, score, health, game_over)
         handle_movement(player_rect)
-        draw_window(player_rect, object_rect, score, health)
+        draw_window(player_rect, object_rect, score, health, game_over)
         clock.tick(FPS)
 
     pygame.quit()
