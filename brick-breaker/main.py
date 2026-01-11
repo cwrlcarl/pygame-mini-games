969f0f739a5498287237e7675a7b8d5a12b79a87
cwrl <29carlganalon@gmail.com>
2026-01-11 16:04:20 +0800

    Handle ball collisions

--- brick-breaker/main.py
+++ brick-breaker/main.py
@@ -5,18 +5,46 @@ from settings import *
 pygame.init()
 
 
-def draw_window(paddle, score):
+def draw_window(score, health, ball, paddle):
     screen.fill(BG_COLOR)
 
-    pygame.draw.rect(screen, PADDLE_COLOR, paddle)
+    score_text = main_window_font.render(f"Score: {score}", True, WHITE)
+    screen.blit(score_text, (15, 15))
 
-    score_text = score_font.render(f"Score: {score}", True, WHITE)
-    screen.blit(score_text, ((SCREEN_WIDTH - score_text.get_width()) // 2, 15))
+    health_text = main_window_font.render(f"Health: {health}", True, WHITE)
+    screen.blit(health_text, (SCREEN_WIDTH - health_text.get_width() - 15, 15))
+
+    pygame.draw.circle(screen, BALL_COLOR, ball.center, BALL_SIZE / 2)
+    pygame.draw.rect(screen, PADDLE_COLOR, paddle)
 
     pygame.display.update()
 
 
-def handle_movement(paddle):
+def handle_ball(ball, ball_dx, ball_dy, paddle, health):
+    ball.x += ball_dx
+    ball.y += ball_dy
+
+    if ball.left <= 0:
+        ball_dx *= -1
+        ball.left = 0
+    if ball.right >= SCREEN_WIDTH:
+        ball_dx *= -1
+        ball.right = SCREEN_WIDTH
+    if ball.top <= 0:
+        ball_dy *= -1
+        ball.top = 0
+
+    if ball.colliderect(paddle):
+        ball_dy *= -1
+
+    if ball.bottom >= SCREEN_HEIGHT:
+        health -= 1
+        spawn_ball(ball)
+
+    return ball_dx, ball_dy, health
+
+
+def handle_paddle(paddle):
     keys = pygame.key.get_pressed()
 
     if (keys[pygame.K_a] or keys[pygame.K_LEFT]) and paddle.x > 0:
@@ -25,12 +53,24 @@ def handle_movement(paddle):
         paddle.x += PADDLE_SPEED
 
 
+def spawn_ball(ball):
+    ball.x = (SCREEN_WIDTH - BALL_SIZE) // 2
+    ball.y = SCREEN_HEIGHT - 70
+
+
 def main():
     paddle_x = (SCREEN_WIDTH - PADDLE_WIDTH) // 2
     paddle_y = SCREEN_HEIGHT - 40
     paddle = pygame.Rect(paddle_x, paddle_y, PADDLE_WIDTH, PADDLE_HEIGHT)
 
+    ball = pygame.Rect(0, 0, BALL_SIZE, BALL_SIZE)
+    spawn_ball(ball)
+
+    ball_dx = random.choice([-4, 4])
+    ball_dy = -5
+
     score = 0
+    health = 3
 
     pygame.mixer.music.play(-1)
     clock = pygame.time.Clock()
@@ -43,8 +83,9 @@ def main():
             if event.type == pygame.QUIT:
                 running = False
 
-        handle_movement(paddle)
-        draw_window(paddle, score)
+        handle_paddle(paddle)
+        ball_dx, ball_dy, health = handle_ball(ball, ball_dx, ball_dy, paddle, health)
+        draw_window(score, health, ball, paddle)
 
     pygame.quit()
 
