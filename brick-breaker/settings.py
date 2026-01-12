3a355457ba0322ec1b2c60d9513d220cda33d841
cwrl <29carlganalon@gmail.com>
2026-01-12 19:23:02 +0800

    Add brick properties

--- brick-breaker/settings.py
+++ brick-breaker/settings.py
@@ -4,18 +4,16 @@ import os
 pygame.init()
 
 # window
-SCREEN_WIDTH, SCREEN_HEIGHT = 500, 500
+SCREEN_WIDTH, SCREEN_HEIGHT = 540, 540
 FPS = 60
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Brick Breaker 🧱")
 
-
 # texts
-score_font = pygame.font.SysFont("Monocraft", 20)
+main_window_font = pygame.font.SysFont("Monocraft", 17)
 game_over_font = pygame.font.SysFont("Monocraft", 80)
 subtext_font = pygame.font.SysFont("Monocraft", 12)
-
 
 # sounds
 BASE_DIR = os.path.dirname(__file__)
@@ -24,21 +22,26 @@ bg_music = os.path.join(ASSETS_DIR, 'bg-music.mp3')
 game_over_sfx = os.path.join(ASSETS_DIR, 'game-over-sfx.ogg')
 
 pygame.mixer.music.load(bg_music)
-pygame.mixer.music.set_volume(0.3)
+pygame.mixer.music.set_volume(0.1)
 game_over_sfx = pygame.mixer.Sound(game_over_sfx)
-
 
 # colors
 BG_COLOR = (37, 35, 41)
 WHITE = (242, 240, 245)
-BALL_COLOR = (214, 56, 56)
+SUBTEXT_COLOR = (184, 181, 189)
+BRICK_COLOR = (116, 58, 232)
+BALL_COLOR = (204, 78, 78)
 PADDLE_COLOR = WHITE
 
+#bricks
+BRICK_WIDTH, BRICK_HEIGHT = 60, 25
+BRICK_GAP = 5
+BRICK_ROW = 5
+BRICK_COLUMN = 7
 
 # paddle
-PADDLE_WIDTH, PADDLE_HEIGHT = 80, 10
+PADDLE_WIDTH, PADDLE_HEIGHT = 90, 10
 PADDLE_SPEED = 5
 
-
 # ball
-BALL_SIZE = 20
+BALL_SIZE = 25
