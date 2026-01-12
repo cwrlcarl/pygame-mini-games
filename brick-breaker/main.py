3b0daac03031bbc6a687a3abe9dd1a9c2acf25d1
cwrl <29carlganalon@gmail.com>
2026-01-12 19:21:38 +0800

    Add bricks and basic collision

--- brick-breaker/main.py
+++ brick-breaker/main.py
@@ -7,11 +7,14 @@ pygame.init()
 def draw_screen(game_state):
     screen.fill(BG_COLOR)
 
-    score_text = main_window_font.render(f"Score: {game_state['score']}", True, WHITE)
+    score_text = main_window_font.render(f"Score: {game_state['score']:04d}", True, WHITE)
     screen.blit(score_text, (15, 15))
 
     health_text = main_window_font.render(f"Health: {game_state['health']}", True, WHITE)
     screen.blit(health_text, (SCREEN_WIDTH - health_text.get_width() - 15, 15))
+
+    for brick in game_state['bricks']:
+        pygame.draw.rect(screen, BRICK_COLOR, brick)
 
     pygame.draw.circle(screen, BALL_COLOR, game_state['ball'].center, BALL_SIZE / 2)
     pygame.draw.rect(screen, PADDLE_COLOR, game_state['paddle'])
@@ -34,14 +37,31 @@ def show_game_over():
     screen.blit(instructions_text, (instructions_text_x, instructions_text_y))
 
 
-def handle_ball(game_state):
+def handle_ball_collisions(game_state):
     if game_state['move_ball']:
+        bricks = game_state['bricks']
         paddle = game_state['paddle']
         ball = game_state['ball']
 
         ball.x += game_state['ball_dx']
         ball.y += game_state['ball_dy']
 
+        # brick collision
+        for brick in bricks:
+            if ball.colliderect(brick):
+                game_state['score'] += 50
+                game_state['ball_dy'] *= -1
+                bricks.remove(brick)
+
+        # paddle collision
+        if ball.colliderect(paddle):
+            if game_state['ball_dy'] > 0:
+                ball.bottom = paddle.top
+                game_state['ball_dy'] *= -1
+            else:
+                game_state['ball_dy'] *= -1
+
+        # wall collision
         if ball.left <= 0:
             game_state['ball_dx'] *= -1
             ball.left = 0
@@ -51,14 +71,6 @@ def handle_ball(game_state):
         if ball.top <= 0:
             game_state['ball_dy'] *= -1
             ball.top = 0
-
-        if ball.colliderect(paddle):
-            if game_state['ball_dy'] > 0:
-                ball.bottom = paddle.top
-                game_state['ball_dy'] *= -1
-            else:
-                game_state['ball_dy'] *= -1
-    
         if ball.top >= SCREEN_HEIGHT:
             game_state['health'] -= 1
 
@@ -104,6 +116,17 @@ def respawn_ball(game_state):
 
 def create_game_state():
     pygame.mixer.music.play(-1)
+    
+    row_width = (BRICK_WIDTH * BRICK_COLUMN + BRICK_GAP * (BRICK_COLUMN - 1))
+    bricks = []
+    
+    for row in range(BRICK_ROW):
+        for col in range(BRICK_COLUMN):
+            brick_x = col * (BRICK_WIDTH + BRICK_GAP) + (SCREEN_WIDTH - row_width) // 2
+            brick_y = row * (BRICK_HEIGHT + BRICK_GAP) + 50
+
+            brick = pygame.Rect(brick_x, brick_y, BRICK_WIDTH, BRICK_HEIGHT)
+            bricks.append(brick)
 
     paddle_x = (SCREEN_WIDTH - PADDLE_WIDTH) // 2
     paddle_y = SCREEN_HEIGHT - 40
@@ -112,6 +135,7 @@ def create_game_state():
     ball = pygame.Rect(0, 0, BALL_SIZE, BALL_SIZE)
 
     game_state = {
+        'bricks': bricks,
         'paddle': paddle,
         'ball': ball,
         'ball_dx': random.choice([-4, 4]),
@@ -151,7 +175,7 @@ def main():
         
         if not game_state['game_over']:
             handle_entities(game_state)
-            handle_ball(game_state)
+            handle_ball_collisions(game_state)
             
         draw_screen(game_state)
 
