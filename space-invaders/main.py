d918347eaa33b209f98d76a101471f38c8a0586b
cwrl <29carlganalon@gmail.com>
2026-01-18 23:27:32 +0800

    Add multiple enemies and collision detection

--- space-invaders/main.py
+++ space-invaders/main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 from settings import *
 
 class Player:
@@ -15,7 +16,6 @@ class Player:
             self.rect.x -= self.speed
         if (keys[pygame.K_d] or keys[pygame.K_RIGHT]) and self.rect.right < SCREEN_WIDTH:
             self.rect.x += self.speed
-
     def draw(self, screen):
         screen.blit(self.img, self.rect)
 
@@ -34,6 +34,19 @@ class Bullet:
         screen.blit(self.img, self.rect)
 
 
+class Enemy:
+    def __init__(self, x, y):
+        self.img = random.choice(ENEMY_IMGS)
+        self.rect = self.img.get_rect()
+        self.rect.center = (x, y)
+
+    def update(self):
+        pass
+
+    def draw(self, screen):
+        screen.blit(self.img, self.rect)
+
+
 def main():
     pygame.init()
     pygame.mixer.music.play(-1)
@@ -43,6 +56,18 @@ def main():
 
     player = Player(x=SCREEN_WIDTH//2, y=SCREEN_HEIGHT-PLAYER_HEIGHT)
     bullets = []
+    enemies = []
+
+    row_width = (ENEMY_WIDTH * ENEMY_COLS) + (ENEMY_ROW_GAP * (ENEMY_COLS - 1))
+    start_x = (SCREEN_WIDTH - row_width) // 2 + ENEMY_WIDTH // 2
+
+    for row in range(ENEMY_ROWS):
+        for col in range(ENEMY_COLS):
+            x = col * (ENEMY_WIDTH + ENEMY_ROW_GAP) + start_x
+            y = row * (ENEMY_HEIGHT + ENEMY_COL_GAP) + ENEMY_OFFSET
+
+            enemy = Enemy(x=x, y=y)
+            enemies.append(enemy)
 
     clock = pygame.time.Clock()
     running = True
@@ -50,6 +75,7 @@ def main():
     while running:
         clock.tick(FPS)
 
+        # events
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
@@ -61,18 +87,28 @@ def main():
                         bullet = Bullet(x=player.rect.centerx, y=player.rect.y)
                         bullets.append(bullet)
 
-        player.update()
-        for bullet in bullets:
-            bullet.update()
+        for bullet in bullets[:]:
+            for enemy in enemies[:]:
+                if bullet.rect.colliderect(enemy.rect):
+                    bullets.remove(bullet)
+                    enemies.remove(enemy)
 
         for bullet in bullets[:]:
             if bullet.rect.y < 0:
                 bullets.remove(bullet)
 
         screen.fill(BG_COLOR)
+
+        player.update()
         player.draw(screen)
+
         for bullet in bullets:
+            bullet.update()
             bullet.draw(screen)
+
+        for enemy in enemies:
+            enemy.update()
+            enemy.draw(screen)
 
         pygame.display.update()
 
