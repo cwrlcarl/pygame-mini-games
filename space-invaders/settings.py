afbf541bb5e17a9ad996fc8a7921430e1308f143
cwrl <29carlganalon@gmail.com>
2026-01-22 15:22:24 +0800

    Add settings for enemy bullet

--- space-invaders/settings.py
+++ space-invaders/settings.py
@@ -2,6 +2,7 @@ import pygame
 import os
 
 pygame.mixer.init()
+pygame.font.init()
 
 SCREEN_WIDTH, SCREEN_HEIGHT = 540, 640
 FPS = 60
@@ -9,6 +10,9 @@ FPS = 60
 # COLORS
 BG_COLOR = (37, 35, 41)
 WHITE = (242, 240, 245)
+
+# TEXTS
+main_window_text = pygame.font.SysFont("Monocraft", 18)
 
 # PATHS
 BASE_DIR = os.path.dirname(__file__)
@@ -35,8 +39,8 @@ _player_img = pygame.image.load(PLAYER_IMG_PATH)
 PLAYER_IMG = pygame.transform.scale_by(_player_img, 0.75)
 PLAYER_WIDTH, PLAYER_HEIGHT = PLAYER_IMG.get_size()
 
-# BULLET
-BULLET_SPEED = 7
+# PLAYER BULLET
+BULLET_SPEED = 6
 MAX_BULLETS = 3
 BULLET_IMG_PATH = os.path.join(IMG_DIR, 'green-laser.png')
 _bullet_img = pygame.image.load(BULLET_IMG_PATH)
@@ -56,3 +60,10 @@ ENEMY_IMGS = [
     for i in range(1, 5)
 ]
 ENEMY_WIDTH, ENEMY_HEIGHT = ENEMY_IMGS[0].get_size()
+
+# ENEMY BULLET
+ENEMY_BULLET_SPEED = 2
+ENEMY_BULLET_IMG_PATH = os.path.join(IMG_DIR, 'red-laser.png')
+_enemy_bullet_img = pygame.image.load(ENEMY_BULLET_IMG_PATH)
+ENEMY_BULLET_IMG = pygame.transform.scale_by(_enemy_bullet_img, 0.30)
+ENEMY_BULLET_WIDTH, ENEMY_BULLET_HEIGHT = ENEMY_BULLET_IMG.get_size()
