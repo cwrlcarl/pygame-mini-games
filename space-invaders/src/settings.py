d1b324c55386b8d095c7b6b80af3679823379033
cwrl <29carlganalon@gmail.com>
2026-02-01 19:40:30 +0800

    Add health UI icon

--- space-invaders/src/settings.py
+++ space-invaders/src/settings.py
@@ -12,11 +12,11 @@ BG_COLOR = (37, 35, 41)
 WHITE = (242, 240, 245)
 
 # TEXTS
-MAIN_FONT = pygame.font.SysFont("Monocraft", 18)
-GAME_OVER_FONT = pygame.font.SysFont("Monocraft", 60)
+MAIN_FONT = pygame.font.SysFont("KenVector Future Regular", 24)
+GAME_OVER_FONT = pygame.font.SysFont("KenVector Future Regular", 60)
 
 # PATHS
-BASE_DIR = os.path.dirname(__file__)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 ASSETS_DIR = os.path.join(BASE_DIR, 'assets')
 IMG_DIR = os.path.join(ASSETS_DIR, 'img')
 AUDIO_DIR = os.path.join(ASSETS_DIR, 'audio')
@@ -39,12 +39,24 @@ HIT_SFX = pygame.mixer.Sound(HIT_SFX_PATH)
 GAME_OVER_SFX_PATH = os.path.join(AUDIO_DIR, 'game-over-sfx.ogg')
 GAME_OVER_SFX = pygame.mixer.Sound(GAME_OVER_SFX_PATH)
 
-# BG IMAGE
-BG_IMAGE_PATH = os.path.join(IMG_DIR, 'black-bg.png')
+# UI ICON
+HEALTH_ICON_PATH =  os.path.join(IMG_DIR, 'player-health.png')
+HEALTH_ICON = pygame.image.load(HEALTH_ICON_PATH)
+HEALTH_ICON_RECT = HEALTH_ICON.get_rect(topleft=(30, 20))
+
+NUMERAL_X_PATH = os.path.join(IMG_DIR, 'numeralx.png')
+NUMERAL_X = pygame.image.load(NUMERAL_X_PATH)
+NUMERAL_X_RECT = NUMERAL_X.get_rect(midleft=(HEALTH_ICON_RECT.right + 10, HEALTH_ICON_RECT.centery))
+
+NUMERAL_IMGS = [
+    pygame.image.load(os.path.join(IMG_DIR, f'numeral{i}.png'))
+    for i in range(4)
+]
+NUMERAL_IMGS_RECT = NUMERAL_IMGS[0].get_rect(midleft=(NUMERAL_X_RECT.right + 10, HEALTH_ICON_RECT.centery))
 
 # PLAYER
 PLAYER_SPEED = 5
-PLAYER_IMG_PATH = os.path.join(IMG_DIR, 'blue-ship.png')
+PLAYER_IMG_PATH = os.path.join(IMG_DIR, 'red-ship.png')
 _player_img = pygame.image.load(PLAYER_IMG_PATH)
 PLAYER_IMG = pygame.transform.scale_by(_player_img, 0.75)
 PLAYER_WIDTH, PLAYER_HEIGHT = PLAYER_IMG.get_size()
@@ -80,3 +92,16 @@ ENEMY_BULLET_IMG_PATH = os.path.join(IMG_DIR, 'red-laser.png')
 _enemy_bullet_img = pygame.image.load(ENEMY_BULLET_IMG_PATH)
 ENEMY_BULLET_IMG = pygame.transform.scale_by(_enemy_bullet_img, 0.30)
 ENEMY_BULLET_WIDTH, ENEMY_BULLET_HEIGHT = ENEMY_BULLET_IMG.get_size()
+
+# METEOR
+METEOR_SPEED = 1
+METEOR_SPAWN_INTERVAL = 3000
+MAX_METEORS = 7
+METEOR_IMGS = [
+    pygame.image.load(os.path.join(IMG_DIR, f'grey-meteor{i}.png'))
+    for i in range(8)
+]
+METEOR_WIDTH, METEOR_HEIGHT = METEOR_IMGS[0].get_size()
+
+# BG IMAGE
+BG_IMAGE_PATH = os.path.join(IMG_DIR, 'black-bg.png')
