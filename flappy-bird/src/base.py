673b56488b3074b2cd98b0bb42c735819595a2bc
cwrl <29carlganalon@gmail.com>
2026-02-06 20:15:22 +0800

    Implement scrolling ground

---
+++ flappy-bird/src/base.py
@@ -0,0 +1,29 @@
+import pygame
+from settings import load_assets, SCREEN_WIDTH, SCREEN_HEIGHT
+
+class Base:
+    def __init__(self):
+        tile = load_assets()['sprites']['base']
+        tile_width = tile.get_width()
+        self.image = pygame.Surface((SCREEN_WIDTH * 2, tile_width))
+
+        for x in range(0, SCREEN_WIDTH * 2, tile_width):
+            self.image.blit(tile, (x, 0))
+
+        self.x1 = 0
+        self.x2 = SCREEN_WIDTH
+        self.y = SCREEN_HEIGHT - 100
+        self.speed = 3
+
+    def update(self):
+        self.x1 -= self.speed
+        self.x2 -= self.speed
+
+        if self.x1 + SCREEN_WIDTH < 0:
+            self.x1 = SCREEN_WIDTH
+        if self.x2 + SCREEN_WIDTH < 0:
+            self.x2 = SCREEN_WIDTH
+
+    def draw(self, screen):
+        screen.blit(self.image, (self.x1, self.y))
+        screen.blit(self.image, (self.x2, self.y))
