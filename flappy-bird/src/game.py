dbc9cea2224a056d324e02177120255c20eb6fb4
cwrl <29carlganalon@gmail.com>
2026-02-06 20:23:30 +0800

    Update and draw base sprite

--- flappy-bird/src/game.py
+++ flappy-bird/src/game.py
@@ -1,4 +1,5 @@
 import pygame
+from base import Base
 from bird import Bird
 from pipe import Pipe
 from settings import *
@@ -9,42 +10,45 @@ class Game:
         self.assets = load_assets()
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
         pygame.display.set_caption("Flappy Bird 🐤")
-
         self.clock = pygame.time.Clock()
         self.running = True
 
         self.bird = Bird()
-
         self.pipes = []
         self.pipe_gap = 250
         self.last_pipe_x = SCREEN_WIDTH
         self.spawn_pipe()
-
-    def update(self):
-        self.bird.update()
-
-        for pipe in self.pipes:
-            pipe.update()
-
-        if len(self.pipes) == 0 or self.pipes[-1].x < SCREEN_WIDTH - self.pipe_gap:
-            self.spawn_pipe()
-
-        for pipe in self.pipes[:]:
-            if pipe.rect.right < 0:
-                self.pipes.remove(pipe)
+        self.base = Base()
 
     def spawn_pipe(self):
         pipe = Pipe(SCREEN_WIDTH)
         self.pipes.append(pipe)
         self.last_pipe_x += SCREEN_WIDTH + self.pipe_gap
 
+    def check_collisions(self):
+        pass
+
+    def update(self):
+        self.bird.update()
+
+        for pipe in self.pipes:
+            pipe.update()
+        if len(self.pipes) == 0 or self.pipes[-1].x < SCREEN_WIDTH - self.pipe_gap:
+            self.spawn_pipe()
+        for pipe in self.pipes[:]:
+            if pipe.rect.right < 0:
+                self.pipes.remove(pipe)
+
+        self.base.update()
+
     def draw(self):
         self.screen.blit(load_assets()['sprites']['background'].convert(), (0, 0))
-        self.bird.draw(self.screen)
 
         for pipe in self.pipes:
             pipe.draw(self.screen)
 
+        self.base.draw(self.screen)
+        self.bird.draw(self.screen)
         pygame.display.update()
 
     def run(self):
