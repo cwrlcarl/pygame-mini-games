85d599df5f3b57a232f442b99ead20fdc4df1157
cwrl <29carlganalon@gmail.com>
2026-02-10 21:54:39 +0800

    Adjust bird physics

--- flappy-bird/src/bird.py
+++ flappy-bird/src/bird.py
@@ -5,16 +5,21 @@ class Bird:
         self.image = load_assets()['sprites']['bird']
         self.rect = self.image.get_rect()
         self.rect.center = (SCREEN_WIDTH // 4, SCREEN_HEIGHT // 2)
+        self.y = float(self.rect.centery)
         self.color = (255, 255, 0)
         self.velocity = 0
-        self.gravity = 0.5
-        self.jump_strength = -9
+        self.gravity = 0.4
+        self.jump_strength = -7.5
+        self.max_fall_speed = 10
         self.is_jumping = False
 
     def update(self):
         if self.is_jumping:
             self.velocity += self.gravity
-            self.rect.y += self.velocity
+            if self.velocity > self.max_fall_speed:
+                self.velocity = self.max_fall_speed
+            self.y += self.velocity
+            self.rect.y = int(self.y)
 
     def draw(self, screen):
         screen.blit(self.image, self.rect)
