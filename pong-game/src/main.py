3464d522017e5b381b5f9b9e723113723e021cbe
cwrl <29carlganalon@gmail.com>
2026-02-24 21:47:42 +0800

    Update ball initial spawn delay

--- pong-game/src/main.py
+++ pong-game/src/main.py
@@ -24,13 +24,14 @@ class Game:
         self._initialize_game_state()
 
     def _initialize_game_state(self):
-        self.starting_y = 55
+        self.ball = None
         self.paddle1 = Paddle(50, SCREEN_HEIGHT // 2, PLAYER_PADDLE_COLOR)
         self.paddle2 = Paddle(SCREEN_WIDTH - 50, SCREEN_HEIGHT // 2, ENEMY_PADDLE_COLOR)
-        self._spawn_ball()
+        self.starting_y = 55
         self.player_score = 0
         self.enemy_score = 0
         self.winning_score = 5
+        self.start_time = pygame.time.get_ticks()
         self.game_over = False
 
     def draw_line(self):
@@ -42,18 +43,18 @@ class Game:
 
     def display_score_text(self):
         player_score = SCORE_FONT.render(f'{self.player_score}', True, WHITE)
-        self.screen.blit(player_score, ((SCREEN_WIDTH - player_score.get_width()) // 4, self.starting_y))
+        self.screen.blit(player_score, ((SCREEN_WIDTH // 2 - player_score.get_width()) // 2, self.starting_y))
         enemy_score = SCORE_FONT.render(f'{self.enemy_score}', True, WHITE)
-        self.screen.blit(enemy_score, ((SCREEN_WIDTH - enemy_score.get_width()) // 4 * 3, self.starting_y))
+        self.screen.blit(enemy_score, (SCREEN_WIDTH // 2 + (SCREEN_WIDTH // 2 - enemy_score.get_width()) // 2, self.starting_y))
 
     def display_winner_text(self):
         if self.game_over:
             result1 = "Win!" if self.player_score == self.winning_score else "Lose!"
             result2 = "Win!" if self.enemy_score == self.winning_score else "Lose!"
             player_side = SCORE_FONT.render(f'{result1}', True, WHITE)
-            self.screen.blit(player_side, ((SCREEN_WIDTH - player_side.get_width()) // 4, self.starting_y + 70))
+            self.screen.blit(player_side, ((SCREEN_WIDTH // 2 - player_side.get_width()) // 2, self.starting_y + 100))
             enemy_side = SCORE_FONT.render(f'{result2}', True, WHITE)
-            self.screen.blit(enemy_side, ((SCREEN_WIDTH - enemy_side.get_width()) // 4 * 3, self.starting_y + 70))
+            self.screen.blit(enemy_side, (SCREEN_WIDTH // 2 + (SCREEN_WIDTH // 2 - enemy_side.get_width()) // 2, self.starting_y + 100))
 
     def draw(self):
         self.screen.fill(BG_COLOR)
@@ -62,7 +63,8 @@ class Game:
         self.display_winner_text()
         self.paddle1.draw(self.screen)
         self.paddle2.draw(self.screen)
-        self.ball.draw(self.screen)
+        if self.ball is not None:
+            self.ball.draw(self.screen)
         pygame.display.flip()
 
     def _spawn_ball(self):
@@ -78,37 +80,41 @@ class Game:
             self.start_time = None
 
     def handle_score(self):
-        if self.ball.rect.right < 0:
-            if not self.ball.scored:
-                self.ball.scored = True
-                self.enemy_score += 1
-            if not self.game_over:
-                self.respawn_ball()
-
-        if self.ball.rect.left > SCREEN_WIDTH:
-            if not self.ball.scored:
-                self.ball.scored = True
-                self.player_score += 1
-            if not self.game_over:
-                self.respawn_ball()
+        if self.ball is not None:
+            if self.ball.rect.right < 0:
+                if not self.ball.scored:
+                    self.ball.scored = True
+                    self.enemy_score += 1
+                if not self.game_over:
+                    self.respawn_ball()
+            if self.ball.rect.left > SCREEN_WIDTH:
+                if not self.ball.scored:
+                    self.ball.scored = True
+                    self.player_score += 1
+                if not self.game_over:
+                    self.respawn_ball()
 
         if self.player_score == self.winning_score or self.enemy_score == self.winning_score:
             self.game_over = True
 
     def handle_collisions(self):
-        if (self.ball.rect.colliderect(self.paddle1.rect) or
-                self.ball.rect.colliderect(self.paddle2.rect)):
-            self.ball.dx *= -1
-
-        if (self.ball.rect.top < 0 or
-                self.ball.rect.bottom > SCREEN_HEIGHT):
-            self.ball.dy *= -1
+        if self.ball is not None:
+            if (self.ball.rect.colliderect(self.paddle1.rect) or
+                    self.ball.rect.colliderect(self.paddle2.rect)):
+                self.ball.dx *= -1
+            if (self.ball.rect.top < 0 or
+                    self.ball.rect.bottom > SCREEN_HEIGHT):
+                self.ball.dy *= -1
 
     def update(self):
+        if self.ball is None:
+            self.respawn_ball()
+
         if not self.game_over:
             self.paddle1.update_paddle1()
             self.paddle2.update_paddle2()
-            self.ball.update()
+            if self.ball is not None:
+                self.ball.update()
 
     def events(self):
         for event in pygame.event.get():
