30a97e30af181fa9259b5b2a5d354335bc95c262
cwrl <29carlganalon@gmail.com>
2026-02-28 21:21:17 +0800

    Add settings.py for game settings

---
+++ pong-game/src/settings.py
@@ -0,0 +1,13 @@
+import pygame
+
+pygame.font.init()
+
+SCREEN_WIDTH, SCREEN_HEIGHT = 800, 600
+FPS = 60
+TEXT_FONT = pygame.font.SysFont('Monocraft', 50)
+
+WHITE = (235, 237, 245)
+BG_COLOR = (22, 23, 26)
+DIVIDER_COLOR = (64, 64, 64)
+PLAYER_PADDLE_COLOR = (51, 61, 196)
+ENEMY_PADDLE_COLOR = (196, 51, 63)
