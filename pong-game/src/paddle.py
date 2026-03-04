1685f16ee4e969680a9af35e44bdd56881cf7559
cwrl <29carlganalon@gmail.com>
2026-03-04 12:09:56 +0800

    Add versus ai paddle

--- pong-game/src/paddle.py
+++ pong-game/src/paddle.py
@@ -4,9 +4,9 @@ from settings import SCREEN_HEIGHT
 class Paddle:
     def __init__(self, x, y, color):
         self.color = color
-        self.width, self.height = 10, 80
+        self.width, self.height = 11, 90
         self.rect = pygame.Rect(x, y, self.width, self.height)
-        self.speed = 10
+        self.speed = 11
 
     def update_paddle1(self):
         k = pygame.key.get_pressed()
@@ -21,6 +21,14 @@ class Paddle:
             self.rect.y -= self.speed
         if k[pygame.K_DOWN] and self.rect.bottom < SCREEN_HEIGHT:
             self.rect.y += self.speed
+
+    def update_ai(self, ball):
+        if (ball.rect.centery < self.rect.centery - 10
+                and self.rect.top > 0):
+            self.rect.y -= 5
+        elif (ball.rect.centery > self.rect.centery + 10
+                and self.rect.bottom < SCREEN_HEIGHT):
+            self.rect.y += 5
 
     def draw(self, screen):
         pygame.draw.rect(screen, self.color, self.rect)
